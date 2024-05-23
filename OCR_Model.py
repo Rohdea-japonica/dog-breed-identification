15943973a71903bf8f3fcd991a4511781f3075d2
Sijia-Shen <1473700558@qq.com>
2024-05-23 16:58:48 +0800

    0.2.2 修复网络结构的计算错误，成功运行

--- OCR_Model.py
+++ OCR_Model.py
@@ -10,39 +10,39 @@ class OCR_Model(nn.Module):
             nn.Conv2d(3, 64, 4, 2, 1),  # size = 200*200*3
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(4, 2, 0),  # size = 249*249*64
+            nn.MaxPool2d(4, 2, 0),  # size = 100*100*64
             # Conv2
-            nn.Conv2d(64, 128, 4, 2, 1),  # size = 100*100*64
+            nn.Conv2d(64, 128, 3, 2, 0),  # size = 49*49*64
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(4, 2, 0),  # size = 50*50*128
+            nn.MaxPool2d(2, 2, 0),  # size = 24*24*128
             # Conv3
-            nn.Conv2d(128, 256, 2, 2, 0),  # size = 24*24*128
+            nn.Conv2d(128, 256, 2, 1, 0),  # size = 12*12*128
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.MaxPool2d(5, 1, 0),  # size = 12*12*256
+            nn.MaxPool2d(2, 1, 0),  # size = 11*11*256
             # Conv4
-            nn.Conv2d(256, 512, 2, 1, 1),  # size = 8*8*256
+            nn.Conv2d(256, 512, 2, 1, 0),  # size = 10*10*256
             nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.MaxPool2d(4, 2, 1),  # size = 8*8*512
+            nn.MaxPool2d(2, 1, 0),  # size = 9*9*512
             # Conv5
-            nn.Conv2d(512, 1024, 2, 2, 1),  # size = 4*4**512
+            nn.Conv2d(512, 1024, 2, 1, 0),  # size = 8*8*512
             nn.BatchNorm2d(1024),
             nn.ReLU(),
-            nn.MaxPool2d(4, 2, 1),  # size = 4*4*1024  ->  output = 2*2*1024
+            nn.MaxPool2d(3, 2, 1),  # size = 7*7*1024  ->  output = 4*4*1024
         )
         self.dense = nn.Sequential(
             # FC1
-            nn.Linear(4096, 2048),
+            nn.Linear(16384, 4096),
             nn.ReLU(),
             nn.Dropout(0.5),
             # FC2
-            nn.Linear(2048, 1024),
+            nn.Linear(4096, 2048),
             nn.ReLU(),
             nn.Dropout(0.5),
             # FC3
-            nn.Linear(1024, 120),
+            nn.Linear(2048, 120),
         )
 
     def forward(self, x):
