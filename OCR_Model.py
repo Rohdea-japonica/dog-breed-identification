48648a0f1ffed3169683a981ee33aa5c01329608
Sijia-Shen <1473700558@qq.com>
2024-05-23 20:05:32 +0800

    0.2.4 对网络结构进行进一步修改 这次将全连接层的层数减少了，对上方的网络结构重新进行了调整，对卷积层和池化层的设置按照其本身意义进行了重新规划，希望能够提高拟合度吧。 当前任务为改进网络，使其训练效果达到较好的效果。

--- OCR_Model.py
+++ OCR_Model.py
@@ -7,17 +7,17 @@ class OCR_Model(nn.Module):
         super().__init__()
         self.backbone = nn.Sequential(
             # Conv1
-            nn.Conv2d(3, 64, 4, 2, 1),  # size = 200*200*3
+            nn.Conv2d(3, 64, 4, 2, 0),  # size = 200*200*3
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(4, 2, 0),  # size = 100*100*64
+            nn.MaxPool2d(3, 2, 0),  # size = 99*99*64
             # Conv2
             nn.Conv2d(64, 128, 3, 2, 0),  # size = 49*49*64
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2, 0),  # size = 24*24*128
+            nn.MaxPool2d(2, 1, 0),  # size = 24*24*128
             # Conv3
-            nn.Conv2d(128, 256, 2, 1, 0),  # size = 12*12*128
+            nn.Conv2d(128, 256, 3, 2, 0),  # size = 23*23*128
             nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.MaxPool2d(2, 1, 0),  # size = 11*11*256
@@ -27,30 +27,26 @@ class OCR_Model(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(2, 1, 0),  # size = 9*9*512
             # Conv5
-            nn.Conv2d(512, 1024, 2, 1, 0),  # size = 8*8*512
+            nn.Conv2d(512, 512, 2, 1, 0),  # size = 8*8*512
             nn.BatchNorm2d(1024),
             nn.ReLU(),
-            nn.MaxPool2d(3, 2, 1),  # size = 7*7*1024  ->  output = 4*4*1024
+            nn.MaxPool2d(2, 1, 0),  # size = 7*7*512  ->  output = 6*6*512
         )
         self.dense = nn.Sequential(
             # FC1
-            nn.Linear(16384, 8192),
+            nn.Linear(18432, 9216),
             nn.ReLU(),
             nn.Dropout(0.5),
             # FC2
-            nn.Linear(8192, 4096),
+            nn.Linear(9216, 4108),
             nn.ReLU(),
             nn.Dropout(0.5),
             # FC3
-            nn.Linear(4096, 2048),
+            nn.Linear(4108, 2048),
             nn.ReLU(),
             nn.Dropout(0.5),
             # FC4
-            nn.Linear(2048, 1024),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            # FC5
-            nn.Linear(1024, 120),
+            nn.Linear(2048, 120),
         )
 
     def forward(self, x):
