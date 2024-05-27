0.5 添加了test部分的代码 修改了之前的小bug，即softmax函数用于init文件而不是net中。

MyDataset.py
@@ -21,13 +21,24 @@ class MyDataset(Dataset):
         return len(self.images)
 
     def __getitem__(self, index):
-        img, label = self.images[index], self.labels[index]
-        img = Image.open(img).convert("RGB")
-        # ImageNet推荐的RGB通道均值、方差
-        mean = [0.485, 0.456, 0.406]
-        std = [0.229, 0.224, 0.225]
-        tf = transforms.Compose([transforms.ToTensor(), transforms.Resize((200, 200), antialias=True), transforms.Normalize(mean=mean, std=std)])
-        img = tf(img)
-        label = torch.tensor(label)
-        return img, label
-
+        if self.mode == "train" or self.mode == "dev":
+            img, label = self.images[index], self.labels[index]
+            img = Image.open(img).convert("RGB")
+            # ImageNet推荐的RGB通道均值、方差
+            mean = [0.485, 0.456, 0.406]
+            std = [0.229, 0.224, 0.225]
+            tf = transforms.Compose([transforms.ToTensor(), transforms.Resize((200, 200), antialias=True),
+                                     transforms.Normalize(mean=mean, std=std)])
+            img = tf(img)
+            label = torch.tensor(label)
+            return img, label
+        elif self.mode == "test":
+            img = self.images[index]
+            img = Image.open(img).convert("RGB")
+            # ImageNet推荐的RGB通道均值、方差
+            mean = [0.485, 0.456, 0.406]
+            std = [0.229, 0.224, 0.225]
+            tf = transforms.Compose([transforms.ToTensor(), transforms.Resize((200, 200), antialias=True),
+                                     transforms.Normalize(mean=mean, std=std)])
+            img = tf(img)
+            return img

ResNet.py
@@ -63,4 +63,5 @@ class ResNet(nn.Module):
 
         x = F.avg_pool2d(x, 7)
         x = x.view(x.size(0), -1)
-        return self.fc(x)
+        x = self.fc(x)
+        return F.softmax(x)

__init__.py
@@ -23,27 +23,34 @@ def get_dictionary(csv_path):
     return dictionary
 
 
-def getdata(data_path):
-    # 读取训练集文件
-    dict_path = os.path.join(data_path, "labels.csv")
-    img_root = os.path.join(data_path, "train")
-    content = pd.read_csv(dict_path)
-    image_id = content["id"]
-    breed = content["breed"]
-    # 构建标签映射字典
-    dictionary = get_dictionary(dict_path)
-    # 设置文件路径和对应的标签
-    labels = []
-    image_names = []
-    for i in range(len(content)):
-        file_path = image_id[i] + ".jpg"
-        labels.append(dictionary[breed[i]])
-        image_names.append(os.path.join(img_root, file_path))
-    random.seed(2)
-    random.shuffle(image_names)
-    random.seed(2)
-    random.shuffle(labels)
-    return image_names, labels
+def getdata(data_path, module):
+    if module == "train":
+        # 读取训练集文件
+        dict_path = os.path.join(data_path, "labels.csv")
+        img_root = os.path.join(data_path, "train")
+        content = pd.read_csv(dict_path)
+        image_id = content["id"]
+        breed = content["breed"]
+        # 构建标签映射字典
+        dictionary = get_dictionary(dict_path)
+        # 设置文件路径和对应的标签
+        labels = []
+        image_names = []
+        for i in range(len(content)):
+            file_path = image_id[i] + ".jpg"
+            labels.append(dictionary[breed[i]])
+            image_names.append(os.path.join(img_root, file_path))
+        random.seed(2)
+        random.shuffle(image_names)
+        random.seed(2)
+        random.shuffle(labels)
+        return image_names, labels
+    elif module == "test":
+        img_root = os.path.join(data_path, "test")
+        image_names = []
+        for root, sub_folder, file_list in os.walk(img_root):
+            image_names += [os.path.join(root, file_path) for file_path in file_list]
+        return image_names
 
 
 if __name__ == "__main__":
@@ -60,11 +67,15 @@ if __name__ == "__main__":
         device = "cpu"
 
     # 获取训练数据集
-    images, labels = getdata("./data")
+    images, labels = getdata("./data", module)
     train_dataset = MyDataset(images, labels, "train")
     dev_dataset = MyDataset(images, labels, "dev")
     train_loader = DataLoader(train_dataset, batch_size, drop_last=False)
     dev_loader = DataLoader(dev_dataset, batch_size, drop_last=False)
+    # 获取预测数据集
+    test_images = getdata("./data", module)
+    test_dataset = MyDataset(test_images, "", "test")
+    test_loader = DataLoader(test_dataset, batch_size, drop_last=False)
     # 加载模型
     model = ResNet().to(device)
     criterion = nn.CrossEntropyLoss()  # 设置误差函数
@@ -81,7 +92,7 @@ if __name__ == "__main__":
     if module == "train":
         model.train()  # 进入训练模式
         epochs = int(input("请输入训练轮数："))
-         # 开始训练
+        # 开始训练
         for epoch in range(epochs):
             count = 0
             correct = 0
@@ -92,12 +103,13 @@ if __name__ == "__main__":
                 loss = criterion(pred, y.to(device))
                 loss.backward()
                 optimizer.step()  # 参数修改
-                label = pred.softmax().argmax(1)
+                label = pred.argmax(1)
                 for i in range(len(y)):
                     if y[i] == label[i]:
                         correct += 1
             print("Current epoch is :", epoch + pre_epoch + 1, " Train_Accuracy is :", correct / count)
-            state_dict = {"model": model.state_dict(), "optimizer": optimizer.state_dict(), "epoch": epoch + 1 + pre_epoch}
+            state_dict = {"model": model.state_dict(), "optimizer": optimizer.state_dict(),
+                          "epoch": epoch + 1 + pre_epoch}
             torch.save(state_dict, "./model.pt")
         print("Finished!!!")
 
@@ -114,3 +126,25 @@ if __name__ == "__main__":
                     if y[i] == label[i]:
                         correct += 1
             print(" Accuracy is :", correct / count)
+    elif module == "test":
+        labls = []
+        with torch.no_grad():
+            model.eval()  # 进入测试模式
+            count = 0
+            correct = 0
+            for x in test_loader:
+                pred = model(x.to(device))
+                labels += pred.tolist()
+        image_names = []
+        for root, sub_folder, file_list in os.walk("./data/test"):
+            image_names += [file_path for file_path in file_list]
+        dictionary = get_dictionary("./data/labels.csv")
+        dict_key = list(dictionary.keys())
+        # 循环进入所有的标签概率列表
+        num = 0
+        for label in labels:
+            # 每个列表的前面加上图片名
+            para = {"id": image_names[num]}
+            for idx in range(len(label)):
+                para[dict_key[idx]] = label[idx]
+            df = pd.DataFrame([para])